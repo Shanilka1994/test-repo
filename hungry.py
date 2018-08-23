22e5560bf2f3004d0ea105ee8496c1577930cf74
Mayodi <mayodi@fortunaglobal.com>
2018-08-23 15:41:46 +0530

    water

--- hungry.py
+++ hungry.py
@@ -2,5 +2,7 @@ print ("Im hungry")
 hungry=input ("are you hungry")
 if hungry=="yes":
 	print("eat")
-else: 
-	print("die")
+else:
+ thirsty==input ("thirsty?")
+ if thirsty=="yes":
+	print("drink water")
